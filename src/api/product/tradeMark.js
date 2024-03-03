ccd5a09945afe0c5c5a99a4ac2f83db97179a651
Hailie17 <192151452@qq.com>
2024-03-04 01:01:41 +0800

    上传图片组件替换上传接口 和 转换配置 dev-api

--- src/api/product/tradeMark.js
+++ src/api/product/tradeMark.js
@@ -6,3 +6,12 @@ import request from '@/utils/request'
 
 // 获取品牌列表
 export const reqTradeMarkList = (page, limit) => request({ url: `/admin/product/baseTrademark/${page}/${limit}`, method: 'get' })
+
+// 新增 / 修改品牌
+export const reqAddorupdateTradeMark = tradeMark => {
+  if (tradeMark.id) {
+    return request({ url: '/admin/product/baseTrademark/update', method: 'put', data: tradeMark })
+  } else {
+    return request({ url: '/admin/product/baseTrademark/save', method: 'post', data: tradeMark })
+  }
+}
--- src/views/product/tradeMark/index.vue
+++ src/views/product/tradeMark/index.vue
@@ -1,37 +1,32 @@
 <template>
   <div class="">
-    <el-button type="primary" icon="el-icon-plus" style="margin: 10px 0;" @click="showDialog">添加</el-button>
-    <el-table :data="list" style="width: 100%;" border>
+    <el-button type="primary" icon="el-icon-plus" style="margin: 10px 0" @click="showDialog">添加</el-button>
+    <el-table :data="list" style="width: 100%" border>
       <el-table-column type="index" label="序号" width="80px" align="center"></el-table-column>
       <el-table-column prop="tmName" label="品牌名称" width="width"></el-table-column>
       <el-table-column prop="logoUrl" label="品牌LOGO" width="width">
-        <template slot-scope="{row, $index}">
-          <img :src="row.logoUrl" alt="" style="width: 100px; height: 100px;">
+        <template slot-scope="{ row, $index }">
+          <img :src="row.logoUrl" alt="" style="width: 100px; height: 100px" />
         </template>
       </el-table-column>
       <el-table-column prop="prop" label="操作" width="width">
-        <template slot-scope="{row, $index}">
+        <template slot-scope="{ row, $index }">
           <el-button type="warning" icon="el-icon-edit" size="mini" @click="updateTradeMark">修改</el-button>
           <el-button type="primary" icon="el-icon-delete" size="mini">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination style="margin-top: 10px; textAlign: center" :curret-page="page" @size-change="handleSizeChange" @current-change="getPageList" :total="total" :page-size="limit" :page-count="7" :page-sizes="[3,5,10]" layout="prev, pager, next, jumper, ->, sizes, total"></el-pagination>
+    <el-pagination style="margin-top: 10px; textalign: center" :curret-page="page" @size-change="handleSizeChange" @current-change="getPageList" :total="total" :page-size="limit" :page-count="7" :page-sizes="[3, 5, 10]" layout="prev, pager, next, jumper, ->, sizes, total"></el-pagination>
 
     <!-- 对话框 -->
     <el-dialog title="添加品牌" :visible.sync="dialogVisible">
-      <el-form style="width: 80%;">
+      <el-form style="width: 80%" :modal="tmForm">
         <el-form-item label="品牌名称" label-width="100px">
-          <el-input autocomplete="off"></el-input>
+          <el-input autocomplete="off" v-model="tmForm.tmName"></el-input>
         </el-form-item>
         <el-form-item label="品牌LOGO" label-width="100px">
-          <el-upload
-            class="avatar-uploader"
-            action="https://jsonplaceholder.typicode.com/posts/"
-            :show-file-list="false"
-            :on-success="handleAvatarSuccess"
-            :before-upload="beforeAvatarUpload">
-            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+          <el-upload class="avatar-uploader" action="/dev-api/admin/product/fileUpload" :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
           </el-upload>
@@ -47,7 +42,7 @@
 
 <script>
 export default {
-  name: 'tradeMark',
+  name: 'TradeMark',
   data() {
     return {
       page: 1,
@@ -55,7 +50,11 @@ export default {
       total: 0,
       list: [],
       dialogVisible: false,
-      imageUrl: ''
+      imageUrl: '',
+      tmForm: {
+        tmName: '',
+        logoUrl: ''
+      }
     }
   },
   mounted() {
@@ -76,37 +75,54 @@ export default {
     showDialog() {
       this.dialogVisible = true
     },
+
     updateTradeMark() {
       this.dialogVisible = true
     },
-    handleAvatarSuccess(res, file) { },
-    beforeAvatarUpload(file) { }
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw)
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
+    }
   }
 }
 </script>
 
 <style>
+.el-pagination {
+  text-align: center;
+}
 .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
-  }
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
