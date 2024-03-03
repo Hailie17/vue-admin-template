81568b0b04e4cb50ca38a569aeb49ce668583792
Hailie17 <192151452@qq.com>
2024-03-03 23:52:51 +0800

    弹框

--- src/views/product/tradeMark/index.vue
+++ src/views/product/tradeMark/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <el-button type="primary" icon="el-icon-plus" style="margin: 10px 0;">添加</el-button>
+    <el-button type="primary" icon="el-icon-plus" style="margin: 10px 0;" @click="showDialog">添加</el-button>
     <el-table :data="list" style="width: 100%;" border>
       <el-table-column type="index" label="序号" width="80px" align="center"></el-table-column>
       <el-table-column prop="tmName" label="品牌名称" width="width"></el-table-column>
@@ -11,12 +11,37 @@
       </el-table-column>
       <el-table-column prop="prop" label="操作" width="width">
         <template slot-scope="{row, $index}">
-          <el-button type="warning" icon="el-icon-edit" size="mini">修改</el-button>
-          <el-button type="primary" icon="el-icon-delete" size="mini">修改</el-button>
+          <el-button type="warning" icon="el-icon-edit" size="mini" @click="updateTradeMark">修改</el-button>
+          <el-button type="primary" icon="el-icon-delete" size="mini">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
     <el-pagination style="margin-top: 10px; textAlign: center" :curret-page="page" @size-change="handleSizeChange" @current-change="getPageList" :total="total" :page-size="limit" :page-count="7" :page-sizes="[3,5,10]" layout="prev, pager, next, jumper, ->, sizes, total"></el-pagination>
+
+    <!-- 对话框 -->
+    <el-dialog title="添加品牌" :visible.sync="dialogVisible">
+      <el-form style="width: 80%;">
+        <el-form-item label="品牌名称" label-width="100px">
+          <el-input autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="100px">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -28,7 +53,9 @@ export default {
       page: 1,
       limit: 3,
       total: 0,
-      list: []
+      list: [],
+      dialogVisible: false,
+      imageUrl: ''
     }
   },
   mounted() {
@@ -45,9 +72,41 @@ export default {
     handleSizeChange(limit) {
       this.limit = limit
       this.getPageList()
-    }
+    },
+    showDialog() {
+      this.dialogVisible = true
+    },
+    updateTradeMark() {
+      this.dialogVisible = true
+    },
+    handleAvatarSuccess(res, file) { },
+    beforeAvatarUpload(file) { }
   }
 }
 </script>
 
-<style scoped></style>
+<style>
+.avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
