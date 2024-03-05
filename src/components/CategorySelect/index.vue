b3c19b6fbf7156ae8f188fb3950329074e29c242
Hailie17 <192151452@qq.com>
2024-03-05 23:52:46 +0800

    注册全局组件

---
+++ src/components/CategorySelect/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="">
+    <el-form>
+      <el-form-item label="一级分类">
+        <el-select v-model="value" placeholder="请选择">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="二级级分类">
+        <el-select v-model="value" placeholder="请选择">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="三级分类">
+        <el-select v-model="value" placeholder="请选择">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategorySelect'
+}
+</script>
+
+<style scoped></style>
