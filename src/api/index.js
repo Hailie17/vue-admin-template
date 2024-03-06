5b8e7d075d1a5015614aad69bac2561457405145
Hailie17 <192151452@qq.com>
2024-03-06 22:38:16 +0800

    一级分类 接口 获取数据动态 展示

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,10 @@
  * 将四个模块请求的接口函数统一返回
  */
 
-import * as trademark from './product/tradeMark';
-import * as attr from './product/attr';
-import * as spu from './product/attr';
-import * as sku from './product/attr';
+import * as trademark from './product/tradeMark'
+import * as attr from './product/attr'
+import * as spu from './product/spu'
+import * as sku from './product/sku'
 
 // 对外暴露
 export default {
--- src/components/CategorySelect/index.vue
+++ src/components/CategorySelect/index.vue
@@ -3,18 +3,16 @@
     <el-form :inline="true" class="demo-form-inline">
       <el-form-item label="一级分类">
         <el-select v-model="value" placeholder="请选择">
-          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+          <el-option v-for="(item, index) in list1" :key="item.id" :label="item.name" :value="item.name" />
         </el-select>
       </el-form-item>
       <el-form-item label="二级级分类">
         <el-select v-model="value" placeholder="请选择">
-          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+          <el-option v-for="item in list1" :key="item.value" :label="item.label" :value="item.value" />
         </el-select>
       </el-form-item>
       <el-form-item label="三级分类">
-        <el-select v-model="value" placeholder="请选择">
-          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"> </el-option>
-        </el-select>
+        <el-select v-model="value" placeholder="请选择"> <el-option v-for="item in list1" :key="item.value" :label="item.label" :value="item.value" / > </el-select>
       </el-form-item>
     </el-form>
   </div>
@@ -22,7 +20,24 @@
 
 <script>
 export default {
-  name: 'CategorySelect'
+  name: 'CategorySelect',
+  data() {
+    return {
+      list1: [],
+      value: ''
+    }
+  },
+  mounted() {
+    this.getCategoryList()
+  },
+  methods: {
+    async getCategoryList() {
+      const res = await this.$API.attr.reqCategory1List()
+      if (res.code === 200) {
+        this.list1 = res.data
+      }
+    }
+  }
 }
 </script>
 
