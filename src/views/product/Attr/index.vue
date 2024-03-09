5f0efb7209c17d249074c31ed034e501b3ab9057
Hailie17 <192151452@qq.com>
2024-03-09 23:44:39 +0800

    el table 表格

--- src/views/product/Attr/index.vue
+++ src/views/product/Attr/index.vue
@@ -7,6 +7,13 @@
       <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
       <el-table style="width: 100%" border :data="attrList">
         <el-table-column type="index" label="序号" align="center" width="80"></el-table-column>
+        <el-table-column prop="attrName" label="属性名称" align="center" width="200"></el-table-column>
+        <el-table-column prop="prop" label="属性值列表" align="center">
+          <template slot-scope="{ row, $index }">
+            <el-tag type="success" v-for="(value, index) in row.attrValueList" :key="value.id" style="margin: 0 20px">{{ value.valueName }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column type="index" label="操作" align="center" width="200"></el-table-column>
       </el-table>
     </el-card>
   </div>
@@ -38,7 +45,7 @@ export default {
       }
     },
     async getAttrList() {
-      const res = await this.$API.Attr.reqAttrList(this.category1Id, this.category2Id, this.category3Id)
+      const res = await this.$API.attr.reqAttrList(this.category1Id, this.category2Id, this.category3Id)
       if (res.code === 200) {
         this.attrList = res.data
       }
