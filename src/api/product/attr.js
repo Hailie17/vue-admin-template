08c0e19edbbc8f1acee087e6497903a05f0691f5
Hailie17 <192151452@qq.com>
2024-03-10 21:34:09 +0800

    1

--- src/api/product/attr.js
+++ src/api/product/attr.js
@@ -13,5 +13,8 @@ export const reqCategory2List = category1Id => request({ url: `/admin/product/ge
 // 获取三级分类数据
 export const reqCategory3List = category2Id => request({ url: `/admin/product/getCategory3/${category2Id}`, method: 'get' })
 
-// 获取分类下的列表数据
+// 获取平台属性的列表数据
 export const reqAttrList = (category1Id, category2Id, category3Id) => request({ url: `/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`, method: 'get' })
+
+// 添加属性接口
+export const reqAddAttr = data => request({ url: '/admin/product/saveAttrInfo', method: 'post', data })
