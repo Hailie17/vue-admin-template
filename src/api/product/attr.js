c2d9681e2e74ef7589c0b3e2caa10a2b57054b0d
Hailie17 <192151452@qq.com>
2024-03-12 01:05:34 +0800

    11

--- src/api/product/attr.js
+++ src/api/product/attr.js
@@ -17,4 +17,4 @@ export const reqCategory3List = category2Id => request({ url: `/admin/product/ge
 export const reqAttrList = (category1Id, category2Id, category3Id) => request({ url: `/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`, method: 'get' })
 
 // 添加属性接口
-export const reqAddAttr = data => request({ url: '/admin/product/saveAttrInfo', method: 'post', data })
+export const reqAddOrUpdateAttr = data => request({ url: '/admin/product/saveAttrInfo', method: 'post', data })
